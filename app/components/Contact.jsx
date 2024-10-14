f3fc095489e07ced30b15304e66dae69eed06888
Ahmad Junaid <142711670+ahmadJunaid56@users.noreply.github.com>
2024-10-14 13:03:49 +0500

    1

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -3,12 +3,13 @@ import React from "react";
 export default function Contact() {
   return (
     <>
-      <div id="contact" className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 mt-10 px-20 sm:text-left pb-16  pt-10 bg-slate-100">
+      <div id="contact" className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 mt-10 px-4 sm:text-left pb-16  pt-10 bg-slate-100">
         <div>
         <h1 href="/" className="text-green-500 font-bold text-lg">
               <span className="bg-green-500 text-white p-1 rounded">F</span>OODI
             </h1>
-          <p className="pr-16 mt-4 text-md  text-gray-500">
+          <p className="pr-8 mt-4 text-md  text-gray-500">
+            Savor the artistry where every dish is a culinary masterpiece
             Savor the artistry where every dish is a culinary masterpiece
           </p>
         </div>
@@ -39,8 +40,8 @@ export default function Contact() {
           </ul>
         </div>
       </div>
-      <div className="flex justify-center bg-slate-100 pt-2 border-t-2 pb-6">
-        <h1 className="text-gray-600">Copyright &#xa9; 2024 Aj Blogs | All rights reserved</h1>
+      <div className="flex justify-center bg-slate-100 pt-2 border-t-2 pb-4">
+        <h1 className="text-gray-600 text-xs md:text-md">Copyright &#xa9; 2024 Aj Blogs | All rights reserved</h1>
       </div>
     </>
   );
